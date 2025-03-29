f75a69c6b958bc52a7cbe8f902a960b016ff7162
HongNhung4702 <lethihongnhung04072002@gmail.com>
2025-03-29 13:57:41 +0700

    First commit

--- src/main/java/utils/DatabaseConnection.java
+++ src/main/java/utils/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/qlbvvnd?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/demo?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "2308";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
         try {
